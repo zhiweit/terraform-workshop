0d513dd99c936abfd44a3ba18150ca91b52ba792
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-10-08 13:16:56 +0800

    chore: add s3 backend file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "your-terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1" 
+  }
+}
+
 locals {
   prefix = "itsa-singapore"
 }
